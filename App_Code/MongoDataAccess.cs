a5245ced5f72b770cc5b76a1d60d37f2ac488808
Nikola Stevanović <banjac275@users.noreply.github.com>
2017-08-21 03:54:01 +0200

    malo radio oko nekog povezivanja s mongom

---
+++ App_Code/MongoDataAccess.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.Core;
+
+/// <summary>
+/// Summary description for MongoDataAccess
+/// </summary>
+public class MongoDataAccess
+{
+    IMongoClient _client;
+    IMongoDatabase _dbase;
+
+    public MongoDataAccess()
+    {
+        _client = new MongoClient("mongodb://localhost:27017");
+        _dbase = _client.GetDatabase("userdb");
+    }
+
+    public async System.Threading.Tasks.Task<IEnumerable<Workers>> GetWorkersAsync()
+    {
+        var collection = _dbase.GetCollection<Workers>("workers");
+        var users = await collection.Find(Builders<Workers>.Filter.Empty).ToListAsync();
+        return users;
+    }
+
+    public Workers getWorker(ObjectId id)
+    {
+        var res = _dbase.GetCollection<Workers>("workers");
+        var filter = Builders<Workers>.Filter.Eq("_id", id);
+        var result = res.Find(filter);
+        return (Workers) result;
+    }
+
+    public async System.Threading.Tasks.Task<IEnumerable<Workers>> Create(Workers w)
+    {
+        var collection = _dbase.GetCollection<Workers>("workers");
+        await collection.InsertOneAsync(w);
+        IEnumerable<Workers> users = null;
+        return users;
+    }
+}
--- App_Code/Workers.cs
+++ App_Code/Workers.cs
@@ -12,5 +12,15 @@ using RaptorDB.Common;
 /// </summary>
 public class Workers
 {
-    
+    public ObjectId Id { get; set; }
+    [BsonElement("WorkerId")]
+    public int WorkerId { get; set; }
+    [BsonElement("FirstName")]
+    public string FirstName { get; set; }
+    [BsonElement("LastName")]
+    public string LastName { get; set; }
+    [BsonElement("Password")]
+    public string Password { get; set; }
+    [BsonElement("Email")]
+    public string Email { get; set; }
 }
